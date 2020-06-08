f13a5b9bf73d3256313c39f58dadbe7734f822be
louischarles2000 <louischarles2000.com>
2020-06-09 02:35:30 +0300

    Added functionality to back home buttons

--- src/Components/person/PersonOut/PersonOut.js
+++ src/Components/person/PersonOut/PersonOut.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './PersonOut.css';
 import Pic from '../pic/pic';
+import * as firebase from 'firebase';
 
 const PersonOut = props => {
-    console.log(props.timeOut)
+    const [error, setError] = useState(null);
+    const [welcome, setWelcome] = useState(false);
+    console.log(props.timeOut);
+    const dates = new Date();
+    const date = {
+        minutes: dates.getMinutes(),
+        hours: dates.getHours(),
+        date: dates.getDate(),
+        month: dates.getUTCMonth() + 1,
+        year: dates.getFullYear()
+    }
+    let t = (date.hours > 12) ? 'PM' : 'AM';
+    const clock = (date.hours > 12 ? date.hours - 12 : date.hours) + ':' + (date.minutes < 10 ? '0' + date.minutes : date.minutes)+ ' ' + t;
+    
+    const onBackHomeHandler = () => {
+        firebase.database().ref().child(`/people/${props.id}/`).update({reason: '', out: false, timeIn: clock})
+        .then(() => {
+            setWelcome(true);
+            setTimeout(() => {
+                setWelcome(false);
+                props.reload()
+            }, 2000);
+            console.log('Damnnn IT WORKED')
+        }
+        ).catch(err => setError('Network problem, please refresh page'));
+    }
     return (
         <div className={classes.PersonOut}>
             <div className={classes.Person}>
@@ -16,8 +42,14 @@ const PersonOut = props => {
                 <p>Reason for going out:</p>
                 <p>> {props.reason}</p>
             </div>
+            <div className={classes.error}>
+                <p>{error ? error : null}</p>
+            </div>
+            {welcome ? <div className={classes.welcome}>
+                <p>Don't forget to wash your hands!</p>
+            </div> : null}
             <div className={classes.Btn}>
-                <button>Back home</button>
+                <button onClick={onBackHomeHandler}>Back home</button>
             </div>
         </div>
     );
